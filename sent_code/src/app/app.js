671c1e0b09bb78f727e008bc871ad99dedc33ef5
Your Name <you@example.com>
2025-02-20 01:11:17 +0500

    revert

--- sent_code/src/app/app.js
+++ sent_code/src/app/app.js
@@ -2,13 +2,29 @@ const express = require('express')
 const cors = require('cors')
 const connectDB = require("../config/db")
 const userRoutes = require("../routes/userRoutes")
+const hotelsRouter = require("../../../routes/hotels");
+const path = require("path");
 
-require("dotenv").config()
+require('dotenv').config({ path: '../../../.env' });
 connectDB()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static("public"));
+
+app.use("/hotels", hotelsRouter);
+
+// Эндпоинт для index.html
+app.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../../public/index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "../../../public")));
+
+app.get("/welcome_page", (req, res) => {
+    res.sendFile(path.join(__dirname, "../sent_code/src/app/public/welcome_page.html"));
+});
 
 app.use("/api/users", userRoutes)
 
